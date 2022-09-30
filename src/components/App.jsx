ed93bbed249ed1746423fcad4d8775d8257e4175
Max Denishchuk <maks.denishchuk@gmail.com>
2022-09-30 17:13:06 +0300

    ///

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,8 +16,7 @@ export class App extends Component {
   };
 
   countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+    return Object.values(this.state).reduce((acc, mark) => acc + mark);
   };
 
   countPositiveFeedbackPercentage = () => {
